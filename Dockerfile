e84e10e9abdbab56b74de0f7c870a7bdfc1506d6
mblhaunted <matthew.lindsey@logrhythm.com>
2017-10-31 16:43:53 -0600

    svs-jenkins for k8s

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM jenkins/jenkins:lts
+USER root
+RUN apt-get update && apt-get install -y curl git
+# drop back to the regular jenkins user - good practice
+USER jenkins
+ADD provision_jenkins.sh /usr/bin/
+ADD install_jenkins_plugins.sh /usr/bin/
+ADD default_jenkins_plugins /tmp/
---
+++ provision_jenkins.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+###########################
+# provision jenkins on k8s
+###########################
+
+# vars
+: ${CONFIG_DEST=/tmp/jenkins-config}
+: ${CONFIG_BASE=$CONFIG_DEST/jenkins_config}
+: ${DEFAULT_PLUGIN_FP=/tmp/default_jenkins_plugins}
+: ${CUSTOM_PLUGINS:=plugins}
+: ${CUSTOM_MAIN_CONFIG:=config.xml}
+: ${CONFIG_REPO:=https://github.schq.secious.com/Logrhythm/lr-kubernetes.git}
+: ${CUSTOM_PLUGIN_FP:=$CONFIG_BASE/$CUSTOM_PLUGINS}
+: ${CUSTOM_CONFIG_FP:=$CONFIG_BASE/$CUSTOM_MAIN_CONFIG}
+: ${CUSTOM_JOBS_PATH:=$CONFIG_BASE/jobs}
+: ${JENKINS_WD:=/var/jenkins_home}
+
+# functions
+installPlugins() {
+    sed ':a;N;$!ba;s/\n/ /g' $1 | /usr/bin/install_jenkins_plugins.sh
+}
+
+# bootstrap container
+mkdir -p $CONFIG_DEST
+
+# install basic plugins
+if [ -f $DEFAULT_PLUGIN_FP ]; then
+    echo "found default jenkins plugins, installing:"
+    cat $DEFAULT_PLUGIN_FP
+    installPlugins $DEFAULT_PLUGIN_FP
+fi
+
+# clone configuration repository
+git clone $CONFIG_REPO $CONFIG_DEST
+
+# check for custom plugins
+if [ -f $CUSTOM_PLUGIN_FP ]; then
+    echo "found custom jenkins plugins. installing:"
+    cat $CUSTOM_PLUGIN_FP
+    installPlugins $CUSTOM_PLUGIN_FP
+fi
+
+# provision jenkins server with stored configuration
+if [ -f $CUSTOM_CONFIG_FP ]; then
+    cp $CUSTOM_CONFIG_FP $JENKINS_WD
+fi
+
+if [ -f $CUSTOM_JOBS_PATH ]; then
+    cp -R $CUSTOM_JOBS_PATH/* $JENKINS_WD/jobs/
+fi
